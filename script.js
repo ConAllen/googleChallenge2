65c20e8d182c998a99a1f610e0e6496d5d17ba66
Your Name <you@example.com>
2017-12-18 17:19:25 +0000

    arrays

--- script.js
+++ script.js
@@ -1502,3 +1502,277 @@ function hasEnoughPlayers(team) {
 
 var team = ["Oliver Wood", "Angelina Johnson", "Katie Bell", "Alicia Spinnet", "George Weasley", "Fred Weasley", "Harry Potter"];
 console.log(hasEnoughPlayers(team));
+
+
+
+/*
+ * Programming Quiz: Joining the Crew (6-6)
+ */
+
+var captain = "Mal";
+var second = "Zoe";
+var pilot = "Wash";
+var companion = "Inara";
+var mercenary = "Jayne";
+var mechanic = "Kaylee";
+
+var crew = [captain, second, pilot, companion, mercenary, mechanic];
+
+var doctor = "Simon";
+var sister = "River";
+var shepherd = "Book";
+
+// your code goes here
+
+crew.push(doctor, sister, shepherd);
+
+console.log(crew);
+
+
+////////////////// Loop Arrays ////////////////////////////////////////////////
+
+
+var donuts = ["jelly donut", "choco donut", "Boston Cream"];
+
+
+for(var i = 0; i < donuts.length; i ++) {
+     donuts[i] += " Hole";
+     donuts[i] = donuts[i].toUpperCase();
+}
+
+////////////////// for Loop Arrays ////////////////////////////////////////////////
+
+
+
+
+
+
+var donuts = ["jelly donut", "chocolate donut", "glazed donut"];
+
+// regular function
+function printDonuts (donut) {
+
+  donut += " hole";
+  donuts[i] = donuts[i].toUpperCase();
+  console.log(donut);
+
+}
+donuts.forEach(printDonuts);
+
+// inline function
+  donuts.forEach(function(donut) {
+  donut += " hole";
+  donut = donut.toUpperCase();
+  console.log(donut);
+});
+
+
+
+/////////////////////////////////////////////////////////////////////////
+
+
+
+words = ["cat", "in", "hat"];
+
+words.forEach(function(word, num, all) {
+  console.log("Word " + num + " in " + all.toString() + " is " + word);
+});
+
+
+
+
+
+friends = ["con", "chris", "lyn"];
+
+friends.forEach(function( word, num, all) {
+  console.log( "word " + num + " is " + all.toString() + " is " + word);
+})
+
+
+/*
+ * Programming Quiz: Another Type of Loop (6-8)
+ *
+ * Use the existing `test` variable and write a `forEach` loop
+ * that adds 100 to each number that is divisible by 3.
+ *
+ * Things to note:
+ *  - you must use an `if` statement to verify code is divisible by 3
+ *  - you can use `console.log` to verify the `test` variable when you're finished looping
+ */
+
+var test = [12, 929, 11, 3, 199, 1000, 7, 1, 24, 37, 4,
+    19, 300, 3775, 299, 36, 209, 148, 169, 299,
+    6, 109, 20, 58, 139, 59, 3, 1, 139
+];
+
+test.forEach(function( num, index, array ){
+    if( num % 3 === 0) {
+        array[index] = num += 100;
+    }
+});
+console.log(test);
+
+/////////////////////////// for loop //////////////////////////
+
+
+
+var array = [12, 929, 11, 3, 199, 1000, 7, 1, 24, 37, 4,
+    19, 300, 3775, 299, 36, 209, 148, 169, 299,
+    6, 109, 20, 58, 139, 59, 3, 1, 139
+];
+
+for (var i = 0; i <= array.length; i = i + 3){
+  console.log(array[i]);
+
+  if (i === 2){
+    break;
+  }
+}
+
+
+/////////////////////////// forEach loop //////////////////////////
+
+var myArray = [12, 929, 11, 3, 199, 1000, 7, 1, 24, 37, 4,
+    19, 300, 3775, 299, 36, 209, 148, 169, 299,
+    6, 109, 20, 58, 139, 59, 3, 1, 139
+];
+
+myArray.forEach(function( elem ) {
+  console.log( elem );
+});
+
+/////////////////////////// map array loop //////////////////////////
+
+var myArray = [12, 929, 11, 3, 199, 1000, 7, 1, 24, 37, 4,
+    19, 300, 3775, 299, 36, 209, 148, 169, 299,
+    6, 109, 20, 58, 139, 59, 3, 1, 139
+];
+
+var newArray = myArray.map(function(elem) {
+
+    elem = elem + 100;
+    return elem;
+});
+
+console.log(newArray)
+
+
+
+
+var donuts = ["jelly donut", "chocolate donut", "glazed donut"]
+
+var newDonut = donuts.map(function( donut ) {
+
+    donut += "HOLE";
+    donut = donut.toUpperCase();
+
+    return donut;
+
+});
+
+console.log(newDonut)
+
+
+/*
+ * Programming Quiz: I Got Bills (6-9)
+ *
+ * Use the .map() method to take the bills array below and create a second array
+ * of numbers called totals. The totals array should contains each amount from the
+ * bills array but with a 15% tip added. Log the totals array to the console.
+ *
+ * For example, the first two entries in the totals array would be:
+ *
+ * [57.76, 21.99, ... ]
+ *
+ * Things to note:
+ *  - each entry in the totals array must be a number
+ *  - each number must have an accuracy of two decimal points
+ */
+
+var bills = [50.23, 19.12, 34.01,
+    100.11, 12.15, 9.90, 29.11, 12.99,
+    10.00, 99.22, 102.20, 100.10, 6.77, 2.22
+];
+
+var totals = bills.map(function( tip ){
+
+    tip = tip * 0.15 + tip;
+
+    return Number(tip.toFixed(2));
+});
+
+console.log(totals);
+
+
+////////////////////// 2 D Row arrays ////////////////////////////////
+
+var donutBox = [
+  ["glazed", "chocolate glazed", "cinnamon"],
+  ["powdered", "sprinkled", "glazed cruller"],
+  ["chocolate cruller", "Boston creme", "creme de leche"]
+];
+
+// here, donutBox.length refers to the number of rows of donuts
+for (var row = 0; row < donutBox.length; row++) {
+  console.log(donutBox[row]);
+}
+
+
+///////////////  rows and columns ////////////////
+
+for (var row = 0; row < donutBox.length; row++) {
+  // here, donutBox[row].length refers to the length of the donut array currently being looped over
+  for (var column = 0; column < donutBox[row].length; column++) {
+    console.log(donutBox[row][column]);
+  }
+}
+
+
+
+
+
+
+for ( var row = 0; row < donutBox.length; row ++){
+
+  for (var column = 0; column < donut[row].length; column ++){
+    console.log(donutBox[row][column]);
+  }
+}
+
+
+/*
+ * Programming Quiz: Nested Numbers (6-10)
+ *
+ *   - The `numbers` variable is an array of arrays.
+ *   - Use a nested `for` loop to cycle through `numbers`.
+ *   - Convert each even number to the string "even"
+ *   - Convert each odd number to the string "odd"
+ */
+
+var numbers = [
+    [243, 12, 23, 12, 45, 45, 78, 66, 223, 3],
+    [34, 2, 1, 553, 23, 4, 66, 23, 4, 55],
+    [67, 56, 45, 553, 44, 55, 5, 428, 452, 3],
+    [12, 31, 55, 445, 79, 44, 674, 224, 4, 21],
+    [4, 2, 3, 52, 13, 51, 44, 1, 67, 5],
+    [5, 65, 4, 5, 5, 6, 5, 43, 23, 4424],
+    [74, 532, 6, 7, 35, 17, 89, 43, 43, 66],
+    [53, 6, 89, 10, 23, 52, 111, 44, 109, 80],
+    [67, 6, 53, 537, 2, 168, 16, 2, 1, 8],
+    [76, 7, 9, 6, 3, 73, 77, 100, 56, 100]
+];
+
+// your code goes here
+
+for(var row = 0; row < numbers.length; row++ ) {
+
+    for(var column = 0; column < numbers[row].length; column++) {
+
+    if(numbers[row][column] % 2 === 0){
+        numbers[row][column] = "even";
+    } else {
+       numbers[row][column] = "odd";
+    }
+}
+}
+console.log(numbers);
